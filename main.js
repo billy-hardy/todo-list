d621150f66d33bc9327ac2196ad237eb33bb4b43
Billy Hardy <hardywb@leidos.com>
2016-12-12 19:52:52 -0500

    added server and web sockets communication

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ let service = new TodoListService("todolist");
 //defaultData.forEach(function(todoList) {
 //    service.saveTodoList(todoList);
 //});
+var ws = new WebSocket("ws://localhost:8080");
+
---
+++ server/websocketsServer.js
@@ -0,0 +1,15 @@
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        wss.clients.forEach(function(client) {
+            if(ws != client) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.send('something');
+});
