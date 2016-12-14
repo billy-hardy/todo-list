9c697ea2f142de21e8fe3daf8e890731c6e4525a
Billy Hardy <hardywb@leidos.com>
2016-12-13 19:01:11 -0500

    writing to and reading from websocket is now done via observable

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let service = new TodoListService("todolist");
 
-let loc = window.location, new_uri;
-if(loc.protocol === "https:") {
-    new_uri = "wss:";
-} else {
-    new_uri = "ws:";
-}
-new_uri += "//" + loc.host;
-var ws = new WebSocket("wss://morning-spire-54006.herokuapp.com/");
+let openObserver = Rx.Observer.create(function(e) {
+    console.log("Socket opened");
+});
 
+let closingObserver = Rx.Observer.create(function(e) {
+    console.log("Socket is about to close");
+});
+
+let socket = Rx.DOM.fromWebSocket(
+    "wss://morning-spire-54006.herokuapp.com/",
+    null,
+    openObserver,
+    closingObserver);
